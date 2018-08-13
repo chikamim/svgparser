a2dce16bdcccc42c1adb8e7b28c79346e3c6ce18
chikami <chikami@exdit.com>
2018-08-13 15:55:14 +0900

    select relatives and ancestors by specified id

--- element.go
+++ element.go
@@ -3,13 +3,16 @@ package svgparser
 import (
 	"encoding/xml"
 	"path"
-	"strings"
+
+	"github.com/renstrom/shortuuid"
 )
 
 // Element is a representation of an SVG element.
 type Element struct {
+	UUID       string
 	Name       string
 	Attributes map[string]string
+	Parent     *Element
 	Children   []*Element
 	Content    string
 }
@@ -17,6 +20,7 @@ type Element struct {
 // NewElement creates element from decoder token.
 func NewElement(token xml.StartElement) *Element {
 	element := &Element{}
+	element.UUID = shortuuid.New()
 	attributes := make(map[string]string)
 	for _, attr := range token.Attr {
 		key := attr.Name.Local
@@ -58,11 +62,37 @@ func (e *Element) XMLElement() xml.StartElement {
 	attr := []xml.Attr{}
 	for k, v := range e.Attributes {
 		name := xml.Name{"", k}
-		c := strings.Split(k, ":")
-		if len(c) > 1 {
-			name = xml.Name{c[0], c[1]}
-		}
 		attr = append(attr, xml.Attr{name, v})
 	}
 	return xml.StartElement{xml.Name{"", e.Name}, attr}
+}
+
+// Ancestors returns ancestors elements
+func (e *Element) Ancestors() []*Element {
+	ee := []*Element{e.Parent}
+	p := e
+	for p.Parent != nil {
+		p = p.Parent
+		ee = append(ee, p)
+	}
+	return ee
+}
+
+// Descendant returns descendant elements
+func (e *Element) Descendant() []*Element {
+	ee := []*Element{}
+	for _, child := range e.Children {
+		ee = append(ee, child)
+		ee = append(ee, child.Descendant()...)
+	}
+	return ee
+}
+
+// Generations returns all generations
+func (e *Element) Generations() []*Element {
+	ee := []*Element{}
+	ee = append(ee, e.Ancestors()...)
+	ee = append(ee, e)
+	ee = append(ee, e.Descendant()...)
+	return ee
 }
--- find.go
+++ find.go
@@ -1,5 +1,10 @@
 package svgparser
 
+import (
+	"log"
+	"regexp"
+)
+
 // FindID finds the first child with the specified ID.
 func (e *Element) FindID(id string) *Element {
 	for _, child := range e.Children {
@@ -7,6 +12,19 @@ func (e *Element) FindID(id string) *Element {
 			return child
 		}
 		if element := child.FindID(id); element != nil {
+			return element
+		}
+	}
+	return nil
+}
+
+// FindUUIID finds the first child with the specified UUID.
+func (e *Element) FindUUID(uuid string) *Element {
+	for _, child := range e.Children {
+		if child.UUID == uuid {
+			return child
+		}
+		if element := child.FindUUID(uuid); element != nil {
 			return element
 		}
 	}
@@ -23,4 +41,90 @@ func (e *Element) FindAll(name string) []*Element {
 		elements = append(elements, child.FindAll(name)...)
 	}
 	return elements
+}
+
+// FilterIDs filter all children with the given ids.
+func (e *Element) SelectByUUIDs(uuids []string) *Element {
+	c := &Element{}
+
+	u := make(map[string]bool)
+	uniq := []string{}
+	for _, uuid := range uuids {
+		if !u[uuid] {
+			u[uuid] = true
+			uniq = append(uniq, uuid)
+		}
+	}
+
+	for _, uuid := range uniq {
+		if e.UUID == uuid {
+			*c = *e
+			c.Children = []*Element{}
+			break
+		}
+	}
+
+	c.Children = e.selectChildrenByUUIDs(uniq)
+
+	return c
+}
+
+func (e *Element) selectChildrenByUUIDs(uuids []string) []*Element {
+	var elements []*Element
+	for _, child := range e.Children {
+		for _, uuid := range uuids {
+			if child.UUID == uuid {
+				c := &Element{}
+				*c = *child
+				c.Children = []*Element{}
+				c.Children = child.selectChildrenByUUIDs(uuids)
+				elements = append(elements, c)
+			}
+		}
+	}
+	return elements
+}
+
+// FindAllLinkedIDs finds related linked ID recursively
+func FindAllLinkedIDs(r *Element, id string) []string {
+	ids := []string{}
+	f := r.FindID(id)
+	if f == nil {
+		return ids
+	}
+
+	c := f
+	for c.Children != nil {
+		for _, h := range c.Children {
+			for _, id := range h.FindLinkedIDs() {
+				ids = append(ids, id)
+			}
+			c = h
+		}
+	}
+
+	ids = append(ids, f.FindLinkedIDs()...)
+	for _, fid := range ids {
+		if fid == id {
+			continue
+		}
+		ids = append(ids, FindAllLinkedIDs(r, fid)...)
+	}
+	log.Println(ids)
+	return ids
+}
+
+// FindLinkedIDs finds related linked ID
+func (e *Element) FindLinkedIDs() []string {
+	ids := []string{}
+	if id, found := e.Attributes["id"]; found {
+		ids = append(ids, id)
+	}
+	r := regexp.MustCompile("#(\\w+)")
+	for _, v := range e.Attributes {
+		if m := r.FindStringSubmatch(v); len(m) > 0 {
+			ids = append(ids, m[1])
+		}
+	}
+	return ids
 }
