2b2d065bcdbacc0044d707b7a5355fadceb38e13
chikami <chikami@exdit.com>
2018-08-13 09:39:06 +0900

    remove output stdout

--- parser.go
+++ parser.go
@@ -3,10 +3,11 @@ package svgparser
 import (
 	"bytes"
 	"encoding/xml"
-	"fmt"
+
 	"io"
 	"io/ioutil"
 	"strings"
+
 	"golang.org/x/net/html/charset"
 )
 
@@ -130,7 +131,7 @@ func Parse(source io.Reader, validate bool) (*Element, error) {
 	if err != nil {
 		return nil, err
 	}
-	fmt.Printf("Node: %v\n", element.Name)
+
 	if err := element.Decode(decoder); err != nil && err != io.EOF {
 		return nil, err
 	}
